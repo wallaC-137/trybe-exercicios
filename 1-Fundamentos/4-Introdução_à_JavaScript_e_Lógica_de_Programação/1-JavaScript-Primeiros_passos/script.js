1ec56c8d9ae9583ed6c7f858a78b13307a8a2fd8
Wallace <wallace_031@hotmail.com>
2022-09-21 20:08:07 -0300

    exercicios 5 e 6 feitos

--- 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/1-JavaScript-Primeiros_passos/script.js
+++ 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/1-JavaScript-Primeiros_passos/script.js
@@ -47,14 +47,66 @@ const c = 13
 
 // Exercício 4
 
-let value = 0
+// let value = 0
 
-if (value > 0){
-    console.log('Positivo')
-}
-else if (value < 0){
-    console.log('Negativo')
-}
-else{
-    console.log('Zero')
+// if (value > 0){
+//     console.log('Positivo')
+// }
+// else if (value < 0){
+//     console.log('Negativo')
+// }
+// else{
+//     console.log('Zero')
+// }
+
+
+// Exercício 5
+
+// let angleA = 80
+// let angleB = 50
+// let angleC = 50
+
+// let sumOfAngles = angleA + angleB + angleC
+
+// if (typeof (sumOfAngles) != "number"){
+//     console.log('Erro: ângulo inválido')
+// }
+// else if(angleA < 0 || angleB < 0 || angleC < 0 ){
+//     console.log('Erro: ângulo inválido')
+// }
+// else if(sumOfAngles == 180){
+//     console.log('True')
+// }
+// else{
+//     console.log('False')
+// }
+
+
+
+// Exercício 6
+
+let chessPeace = 'CACHORRO' 
+let nameLC = chessPeace.toLowerCase()
+
+switch (nameLC) {
+    case 'cavalo':
+        console.log('O cavalo pode ser movido para uma das casas mais próximas a que ocupa que não esteja na mesma fileira, coluna ou diagonal')
+        break
+    case 'dama':
+        console.log('A dama pode ser movida para qualquer casa ao longo da fileira, coluna ou diagonal em que se encontra. ')
+        break
+    case 'rei':
+        console.log('Pode mover-se em qualquer direção, porém apenas uma casa por vez, como indicam os pontos vermelhos na figura abaixo.')
+        break
+    case 'bispo':
+        console.log('O bispo pode ser movido para qualquer casa ao longo da diagonal em que se encontra.')
+        break
+    case 'torre':
+        console.log('A torre pode ser movida para qualquer casa ao longo da fileira ou coluna em que se encontra. ')
+        break
+    case 'peão':
+        console.log('O peão pode ser movido para uma casa que esteja imediatamente à sua frente ao longo da mesma coluna, desde que esta casa esteja desocupada')
+    default:
+        console.log('Digite um nome de peça válido')
+
 }
