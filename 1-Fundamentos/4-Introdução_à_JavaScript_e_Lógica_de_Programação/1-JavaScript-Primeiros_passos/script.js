0aabd64640ad6bd16ccb64204b052e9c447bce7e
Wallace <wallace_031@hotmail.com>
2022-09-21 14:48:37 -0300

    adiciona exercicio  1 e 2

---
+++ 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/1-JavaScript-Primeiros_passos/script.js
@@ -0,0 +1,28 @@
+const a = 12
+const b = 12
+
+// Exercício 1
+
+let adicao = a+b
+
+let subtracao = a-b
+
+let multiplicacao = a*b
+
+let divisao = a/b
+
+let divisaoSobra = a%b
+
+console.log(divisaoSobra)
+
+
+if (a > b) {
+    console.log('A variável "A" é maior que a "B" ')
+}
+else if (b > a) {
+    console.log('A variável "B" é maior que a "A"')
+}
+else{
+    console.log('A e B tem o mesmo valor')
+}
+
