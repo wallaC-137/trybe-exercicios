2ac63045a238c6f02261819d124a42c273095faf
Wallace <wallace_031@hotmail.com>
2022-09-21 15:14:19 -0300

    exercicios 3 e 4 feitos

--- 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/1-JavaScript-Primeiros_passos/script.js
+++ 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/1-JavaScript-Primeiros_passos/script.js
@@ -1,29 +1,60 @@
-const a = 12
+const a = 15
 const b = 12
-
+const c = 13
 // Exercício 1
 
-let adicao = a+b
+// let adicao = a+b
 
-let subtracao = a-b
+// let subtracao = a-b
 
-let multiplicacao = a*b
+// let multiplicacao = a*b
 
-let divisao = a/b
+// let divisao = a/b
 
-let divisaoSobra = a%b
+// let divisaoSobra = a%b
 
-console.log(divisaoSobra)
+// console.log(divisaoSobra)
+
 
 // Exercício 2
 
-if (a > b) {
-    console.log('A variável "A" é maior que a "B" ')
+// if (a > b) {
+//     console.log('A variável "A" é maior que a "B" ')
+// }
+// else if (b > a) {
+//     console.log('A variável "B" é maior que a "A"')
+// }
+// else{
+//     console.log('A e B tem o mesmo valor')
+// }
+
+
+// // Exercício 3
+
+// if (a > b && a > c) {
+//     console.log('A variável "A" é maior que "B" e "C"!')
+// }
+// else if (b > a && b > c) {
+//     console.log('A variável "B" é maior que "A" e "C"!')
+// }
+// else if (c > a && c > b){
+//     console.log('A variável "C" é maior que "A" e "B"!')
+// }
+// else{
+//     console.log('As tres variáveis tem o mesmo valor')
+// }
+
+
+// Exercício 4
+
+let value = 0
+
+if (value > 0){
+    console.log('Positivo')
 }
-else if (b > a) {
-    console.log('A variável "B" é maior que a "A"')
+else if (value < 0){
+    console.log('Negativo')
 }
 else{
-    console.log('A e B tem o mesmo valor')
+    console.log('Zero')
 }
-
