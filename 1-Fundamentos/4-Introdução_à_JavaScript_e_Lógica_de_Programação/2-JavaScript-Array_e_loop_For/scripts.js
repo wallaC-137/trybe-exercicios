101e7174f8d644f8f18e0e6092e64ad7f4cbf419
Wallace <wallace_031@hotmail.com>
2022-09-22 09:53:57 -0300

    exercicios 1 e 2 feitos

---
+++ 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/2-JavaScript-Array_e_loop_For/scripts.js
@@ -0,0 +1,19 @@
+//exercício 1
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu.indexOf('Serviços');
+
+// console.log(menuServices);
+
+
+
+//exercício 2
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+
+
+//exercício 3
