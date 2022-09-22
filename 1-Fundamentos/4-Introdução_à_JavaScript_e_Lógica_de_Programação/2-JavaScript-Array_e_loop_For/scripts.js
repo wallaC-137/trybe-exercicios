61fa195ec9b99b0eb350a84716e77209dadedcfc
Wallace <wallace_031@hotmail.com>
2022-09-22 10:43:25 -0300

    exercicios 3 e 4 feitos

--- 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/2-JavaScript-Array_e_loop_For/scripts.js
+++ 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/2-JavaScript-Array_e_loop_For/scripts.js
@@ -17,3 +17,25 @@
 
 
 //exercício 3
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// menu.push('Contato')
+
+// console.log(menu);
+
+
+
+//exercício 4
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let i = 0; i < groceryList.length; i += 1) {
+    console.log(groceryList[i])
+}
+
+// de trás pra frente 
+
+for (let i = groceryList.length-1; i >= 0; i -= 1) {
+    console.log(groceryList[i])
+}
