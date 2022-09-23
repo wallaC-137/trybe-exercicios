fdea48a8f81498de6b0ada974d35a6784cb07117
Wallace <wallace_031@hotmail.com>
2022-09-23 10:23:59 -0300

    exercicios 1 e 2 feitos

---
+++ 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/3-JavaScript-Lógica_de_Programação_e_Algoritmos/scripts.js
@@ -0,0 +1,36 @@
+// exercicio 1
+
+// let numFactored = 10;
+// let initialValue = numFactored - 1;
+// let actualValue = null;
+
+// for (let index = initialValue; index > 1 ; index -= 1){
+//  if (actualValue == null){
+//     actualValue = numFactored * index
+//  } else{
+//     actualValue = actualValue * index
+//  }
+//  console.log(actualValue)
+// }
+
+// a forma abaixo seria o gabarito da questão de cima, mas está incorreto
+
+// let fatorial = 10;
+
+// for (let index = 10; index > 0; index -= 1) {
+//   fatorial *= index;
+//   console.log(fatorial);
+// }
+
+
+
+// exercicio 2
+
+// let word = 'tryber';
+// let palindromo = ''
+
+// for (let index = (word.length-1); index >= 0; index -= 1){
+//     palindromo += word[index]
+// }
+
+// console.log(palindromo)
