78467615b9c64500d44264508ddcf1532b4e50db
Wallace <wallace_031@hotmail.com>
2022-09-24 09:21:22 -0300

    exercicios 3 feito

--- 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/3-JavaScript-Lógica_de_Programação_e_Algoritmos/scripts.js
+++ 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/3-JavaScript-Lógica_de_Programação_e_Algoritmos/scripts.js
@@ -34,3 +34,103 @@
 // }
 
 // console.log(palindromo)
+
+
+
+// exercicio 3
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let palavra1= '';
+// let palavra2= '';
+
+// for (let index = 0; index <= array.length; index += 1) {
+//   if(palavra1.length == 0){
+//     palavra1 = array[index]
+//     palavra2 = array[index+1]
+//   } else if (palavra1.length < palavra2.length){
+//     palavra1 = palavra2
+//   } else {
+//     palavra2 = array[index]
+//   }
+
+// }
+
+// console.log(palavra1)
+
+
+// outra forma de fazer
+
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let maiorNum = '';
+// let menorNum = '';
+
+// for (let index = 0; index < array.length; index += 1) {
+//     if (maiorNum.length < array[index].length){
+//         maiorNum = array[index]
+//     }
+// }
+
+// for (let index = 0; index < array.length; index += 1) {
+//     if (menorNum.length > array[index].length || menorNum == 0){
+//         menorNum = array[index]
+//     }
+// }
+
+// console.log(maiorNum)
+// console.log(menorNum)
+
+
+// exercicio 3
+
+let number = 50
+let primeNumbers = []
+
+
+// for (let index = 2; index < number; index += 1) {
+//     for (let index2 = 2; index < index; index2 += 1) {
+//         if (index % index2 > 0) {
+//             con1 += 1
+//         }
+//     }
+//     let con1 = 0
+//     if(con1 == 0) {
+//         primeNumbers.push(index)
+//     }
+// }
+
+// for (let index = 2; index <= number; index += 1) {
+//    let sinal = 0
+//    for (let index2 = 2; index2 < index; index2 += 1) {
+//       if(index % index2 == 0){
+//          sinal += 1
+//       }
+//    }
+//    if(sinal == 0){
+//       primeNumbers.push(index)
+//    }
+// }
+// console.log(primeNumbers)
+
+
+
+for (let index = 2; index <= number; index += 1) {
+   let contador = 0
+   for (let index2 = 2; index2 < index; index2 += 1) {
+      if (index % index2 == 0) {
+         contador += 1
+      }
+   }
+   if (contador == 0) {
+      primeNumbers.push(index)
+   }
+}
+
+let larger = primeNumbers[primeNumbers.length-1]
+
+console.log(`Esses são todos os numeros primos entre 1 e ${number}: ${primeNumbers}. O maior é: ${larger}`)
+
+
+
