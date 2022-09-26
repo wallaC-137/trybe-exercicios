d982f16d3a3cbaa9352b577c0857ff4ce81c5812
Wallace <wallace_031@hotmail.com>
2022-09-26 11:49:30 -0300

    alguns exemplos feitos. Iniciando exercicios do dia

---
+++ 1-Fundamentos/4-Introdução_à_JavaScript_e_Lógica_de_Programação/04-JavaScript-Objetos_e_funções/scripts.js
@@ -0,0 +1,116 @@
+// exercício 1 de fixação
+
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: {
+//         golden: 2,
+//         silver: 3,
+//     }
+
+// }
+
+// console.log('O nome dela é ' + player.name + ' ' + player.lastName + ' ela tem ' + player.age + ' anos de idade e já trouxe ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata')
+
+
+
+
+// exercício 2 de fixação
+
+// function adicao (a, b){
+//     let resultado = a + b
+//     return 'a resultado é: ' + resultado
+// }
+
+// console.log(adicao(8,2))
+
+
+
+// function subtracao (a, b){
+//     let resultado = a - b
+//     return 'a resultado é: ' + resultado
+// }
+
+// console.log(subtracao(10,2))
+
+
+
+// function multiplicacao (a, b){
+//     let resultado = a * b
+//     return 'a resultado é: ' + resultado
+// }
+
+// console.log(multiplicacao(10,3))
+
+
+
+// function divisao (a, b){
+//     let resultado = a / b
+//     return 'a resultado é: ' + resultado
+// }
+
+// console.log(divisao(10,3))
+
+
+
+
+// function divisao (a, b){
+//     let resultado = a / b
+//     return 'a resultado é: ' + resultado
+// }
+
+// console.log(divisao(10,3))
+
+
+
+// function modolo (a, b){
+//     let resultado = a % b
+//     return 'a resultado é: ' + resultado
+// }
+
+// console.log(modolo(10,3))
+
+
+
+// function numberLarge (num1, num2, num3) {
+//     if (num1 > num2 && num1 > num3) {
+//         return num1 + ' é o maior dos 3 números'
+//     } else if (num2 > num1 && num2 > num3){
+//         return num2 + ' é o maior dos 3 números'
+//     } else {
+//         return num3 + ' é o maior dos 3 números'
+//     }
+// }
+
+// console.log(numberLarge(10, 11, 18))
+
+
+
+// function positiveOrNegative (valor){
+//     if(valor > 0){
+//         return 'Positive'
+//     } else if (valor < 0){
+//         return 'Negative'
+//     } else{
+//         return 'Zero'
+//     }
+// }
+
+// console.log(positiveOrNegative(0))
+
+
+// function triangleAngle (angle1, angle2, angle3){
+//     if(typeof angle1 !== 'number' || typeof angle2 !== 'number' || typeof angle3 !== 'number') {
+//         return 'Error'
+//     } else if(angle1 <= 0 || angle2 <= 0 || angle3 <= 0){
+//         return 'Error'
+//     } else if(angle1+angle2+angle3 == 180){
+//         return 'True'
+//     } else{
+//         return 'False'
+//     }
+// }
+
+// console.log(triangleAngle(50, 50, 80))
+
