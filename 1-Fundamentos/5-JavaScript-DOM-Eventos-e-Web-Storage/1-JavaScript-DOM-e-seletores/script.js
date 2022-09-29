b886c1fcbf8fe0b33e6751f830cc3f4de61f7a71
Wallace <wallace_031@hotmail.com>
2022-09-29 07:19:47 -0300

    coneçando exercicios

---
+++ 1-Fundamentos/5-JavaScript-DOM-Eventos-e-Web-Storage/1-JavaScript-DOM-e-seletores/script.js
@@ -0,0 +1,15 @@
+document.querySelector('h3').style.backgroundColor = '#ff6f9c';
+document.querySelector('h3').style.color = '#fff';
+document.querySelector('div').style.backgroundColor = 'pink';
+
+document.querySelectorAll('section div h3')[1].style.backgroundColor = '#ff6f9c';
+document.querySelectorAll('section div h3')[1].style.color = '#fff';
+document.querySelectorAll('section div')[1].style.backgroundColor = 'pink';
+
+document.querySelectorAll('section div h3')[2].style.backgroundColor = 'grey';
+document.querySelectorAll('section div h3')[2].style.color = '#fff';
+document.querySelectorAll('section div')[2].style.backgroundColor = 'yellow';
+
+document.querySelectorAll('section div h3')[3].style.backgroundColor = 'grey';
+document.querySelectorAll('section div h3')[3].style.color = '#fff';
+document.querySelectorAll('section div')[3].style.backgroundColor = 'yellow';
